2450bd25fb876485496a686ab9b0b56c61d46fb1
illtellyoulater <3078931+illtellyoulater@users.noreply.github.com>
2024-03-17 08:37:49 +0100

    Update Gruntfile.js

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -1,29 +1,9 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    // Lint JavaScript files
-    eslint: {
-      target: ['src/**/*.js']
-    },
-
-    // Minify JavaScript files
-    uglify: {
-      my_target: {
-        files: {
-          'dist/app.min.js': ['src/**/*.js']
-        }
-      }
-    },
-
     // Any other build tasks relevant to your project
   });
 
-  // Load the necessary plugins
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-eslint');
-
-  // Define the default task that the GitHub Actions workflow will execute
+    // Define the default task that the GitHub Actions workflow will execute
   grunt.registerTask('build', ['eslint', 'uglify']);
-  grunt.registerTask('default', ['eslint', 'uglify']);
 };
